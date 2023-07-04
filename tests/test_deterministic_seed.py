91b6960c0372fefd7be910e783a3c290dcc5e172
HenningSE <h_schu55@uni-muenster.de>
2023-07-04 10:56:17 -0500

    Add deterministic seed test

---
+++ tests/test_deterministic_seed.py
@@ -0,0 +1,53 @@
+import unittest
+import fuse
+import tempfile
+import numpy as np
+
+class TestDeterministicSeed(unittest.TestCase):
+
+    def setUp(self):
+
+        self.temp_dir_0 = tempfile.TemporaryDirectory()
+        self.temp_dir_1 = tempfile.TemporaryDirectory()
+
+        self.test_context_0 = fuse.context.microphysics_context(self.temp_dir_0.name)
+
+        self.test_context_0.set_config({"path": "/project2/lgrandi/xenonnt/simulations/testing",
+                                      "file_name": "pmt_neutrons_100.root",
+                                      })
+
+        self.test_context_1 = fuse.context.microphysics_context(self.temp_dir_1.name)
+
+        self.test_context_1.set_config({"path": "/project2/lgrandi/xenonnt/simulations/testing",
+                                      "file_name": "pmt_neutrons_100.root",
+                                      })
+        
+        self.run_number_0 = "TestRun_00000"
+        self.run_number_1 = "TestRun_00001"
+
+    def tearDown(self):
+
+        self.temp_dir_0.cleanup()
+        self.temp_dir_1.cleanup()
+
+    def test_MicroPhysics_SameSeed(self):
+        """Test that the same run_number and lineage produce the same random seed and thus the same output"""
+
+        self.test_context_0.make(self.run_number_0, "microphysics_summary")
+        self.test_context_1.make(self.run_number_1, "microphysics_summary")
+
+        output_0 = self.test_context_0.get_array(self.run_number_0, "microphysics_summary")
+        output_1 = self.test_context_1.get_array(self.run_number_0, "microphysics_summary")
+
+        self.assertTrue(np.all(output_0 == output_1))
+
+    def test_MicroPhysics_DifferentSeed(self):
+        """Test that a different run_number produce a different random seed and thus different output"""
+
+        self.test_context_0.make(self.run_number_0, "microphysics_summary")
+        self.test_context_1.make(self.run_number_1, "microphysics_summary")
+
+        output_0 = self.test_context_0.get_array(self.run_number_0, "microphysics_summary")
+        output_1 = self.test_context_1.get_array(self.run_number_1, "microphysics_summary")
+
+        self.assertFalse(np.all(output_0 == output_1))
