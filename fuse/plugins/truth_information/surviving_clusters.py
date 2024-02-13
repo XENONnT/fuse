45d816a78e3eb7bea98ec344d6155e9e221e953c
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2024-02-13 10:57:39 -0600

    Refactor SurvivingClusters

--- fuse/plugins/truth_information/surviving_clusters.py
+++ fuse/plugins/truth_information/surviving_clusters.py
@@ -6,35 +6,35 @@ export, __all__ = strax.exporter()
 @export
 class SurvivingClusters(strax.Plugin):
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
 
-    depends_on = ("peak_basics", "contributing_clusters", "microphysics_summary")
+    depends_on = ("peak_basics", "photon_summary", "microphysics_summary")
     provides = "surviving_clusters"
 
-    dtype = [('in_a_record', np.bool_),
+    dtype = [('creating_a_photon', np.bool_),
              ('in_a_peak', np.bool_),
              ]
     dtype += strax.time_fields
 
     data_kind = "interactions_in_roi"
 
-    def compute(self, peaks, raw_records, interactions_in_roi):
+    def compute(self, peaks, propagated_photons, interactions_in_roi):
 
         #Check if the cluster contributes to any cluster
-        cluster_in_any_record = np.isin(interactions_in_roi["cluster_id"], np.unique(raw_records["contributing_clusters"]))
+        cluster_creating_a_photon = np.isin(interactions_in_roi["cluster_id"], np.unique(propagated_photons["cluster_id"]))
 
-        contributing_clusters_per_peak = strax.split_touching_windows(raw_records, peaks)
+        photons_per_peaks = strax.split_touching_windows(propagated_photons, peaks)
 
         clusters_in_peaks = []
         for i in range(len(peaks)):
-            unique_contributing_clusters = np.unique(contributing_clusters_per_peak[i]["contributing_clusters"])
+            unique_contributing_clusters = np.unique(photons_per_peaks[i]["cluster_id"])
             clusters_in_peaks.append(unique_contributing_clusters)
         clusters_that_make_it_into_a_peak = np.unique(np.concatenate(clusters_in_peaks))
 
         cluster_in_any_peak = np.isin(interactions_in_roi["cluster_id"], clusters_that_make_it_into_a_peak)
 
         result = np.zeros(len(interactions_in_roi), dtype=self.dtype)
-        result['in_a_record'] = cluster_in_any_record
+        result['creating_a_photon'] = cluster_creating_a_photon
         result['in_a_peak'] = cluster_in_any_peak
         result['time'] = interactions_in_roi['time']
         result['endtime'] = interactions_in_roi['endtime']
