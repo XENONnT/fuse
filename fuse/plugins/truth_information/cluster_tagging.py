f656ae7b0587e63e82b376df35cb6ac437b7da0d
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2024-02-13 10:42:16 -0600

    Refactor ClusterTagging

--- fuse/plugins/truth_information/cluster_tagging.py
+++ fuse/plugins/truth_information/cluster_tagging.py
@@ -7,63 +7,58 @@ export, __all__ = strax.exporter()
 class ClusterTagging(strax.Plugin):
     """Plugin to tag if clusters contribute to the main or alternative s1/s2"""
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
 
-    depends_on = ("peak_basics","microphysics_summary", "event_basics", "contributing_clusters")
+    depends_on = ("peak_basics","microphysics_summary", "event_basics", "photon_summary")
     provides = "tagged_clusters"
     data_kind = "interactions_in_roi"
 
     dtype = [('in_main_s1', np.bool_),
-         ('in_main_s2', np.bool_),
-         ('in_alt_s1', np.bool_),
-         ('in_alt_s2', np.bool_),
-        ]
-    dtype = dtype + strax.time_fields   
+             ('in_main_s2', np.bool_),
+             ('in_alt_s1', np.bool_),
+             ('in_alt_s2', np.bool_),
+             ('photons_in_main_s1', np.int32),
+             ('photons_in_main_s2', np.int32),
+             ('photons_in_alt_s1', np.int32),
+             ('photons_in_alt_s2', np.int32),
+            ]
+    dtype = dtype + strax.time_fields    
 
-    def compute(self, peaks, raw_records, interactions_in_roi, events):
+    def compute(self, peaks, interactions_in_roi, events, propagated_photons):
 
-        peaks_in_event= strax.split_by_containment(peaks, events)
-        contributing_clusters_per_event = strax.split_touching_windows(raw_records, events)
+        peaks_in_event = strax.split_by_containment(peaks, events)
+        photon_in_event = strax.split_touching_windows(propagated_photons, events)
 
         result = np.zeros(len(interactions_in_roi), dtype=self.dtype)
         result['time'] = interactions_in_roi['time']
         result['endtime'] = interactions_in_roi['endtime']
 
-        for i, (pic, e, cie) in enumerate(zip(peaks_in_event, events, contributing_clusters_per_event)):
-            s1 = pic[e['s1_index']]
-            s2 = pic[e['s2_index']]
+        for i, (peaks_of_event, event_i, photon_of_event) in enumerate(zip(peaks_in_event, events, photon_in_event)):
 
-            alt_s1 = pic[e['alt_s1_index']]
-            alt_s2 = pic[e['alt_s2_index']]
+            peak_photons = strax.split_touching_windows(photon_of_event, peaks_of_event)
 
-            clusters = strax.split_touching_windows(cie, pic)
+            peak_name_dict = {"main_s1": "s1_index",
+                              "main_s2": "s2_index",
+                              "alt_s1": "alt_s1_index",
+                              "alt_s2": "alt_s2_index",
+                            }
 
-            main_s1_clusters = clusters[e['s1_index']]
-            main_s2_clusters = clusters[e['s2_index']]
-            alt_s1_clusters = clusters[e['alt_s1_index']]
-            alt_s2_clusters = clusters[e['alt_s2_index']]
-            
-            contributing_clusters_main_s1 = np.unique(main_s1_clusters['contributing_clusters'])
-            contributing_clusters_main_s2 = np.unique(main_s2_clusters['contributing_clusters'])
-            contributing_clusters_alt_s1 = np.unique(alt_s1_clusters['contributing_clusters'])
-            contributing_clusters_alt_s2 = np.unique(alt_s2_clusters['contributing_clusters'])
+            for peak_name in peak_name_dict.keys():
 
-            contributing_clusters_main_s1 = contributing_clusters_main_s1[contributing_clusters_main_s1>0]
-            contributing_clusters_main_s2 = contributing_clusters_main_s2[contributing_clusters_main_s2>0]
-            contributing_clusters_alt_s1 = contributing_clusters_alt_s1[contributing_clusters_alt_s1>0]
-            contributing_clusters_alt_s2 = contributing_clusters_alt_s2[contributing_clusters_alt_s2>0]
+                peak_idx = event_i[peak_name_dict[peak_name]]
+                if peak_idx >= 0:
 
+                    photons_of_peak = peak_photons[event_i[peak_name_dict[peak_name]]]
 
-            matching_clusters_main_s1 = np.argwhere(np.isin(interactions_in_roi["cluster_id"], contributing_clusters_main_s1))
-            result["in_main_s1"][matching_clusters_main_s1] = True
+                    contributing_clusters_of_peak, photons_in_peak = np.unique(photons_of_peak['cluster_id'], return_counts=True)
+                    photons_in_peak = photons_in_peak[contributing_clusters_of_peak>0]
+                    contributing_clusters_of_peak = contributing_clusters_of_peak[contributing_clusters_of_peak>0]
 
-            matching_clusters_main_s2 = np.argwhere(np.isin(interactions_in_roi["cluster_id"], contributing_clusters_main_s2))
-            result["in_main_s2"][matching_clusters_main_s2] = True
+                    matching_clusters = np.argwhere(np.isin(interactions_in_roi["cluster_id"], contributing_clusters_of_peak))
+                    result["in_" + peak_name][matching_clusters] = True
 
-            matching_clusters_alt_s1 = np.argwhere(np.isin(interactions_in_roi["cluster_id"], contributing_clusters_alt_s1))
-            result["in_alt_s1"][matching_clusters_alt_s1] = True
-
-            matching_clusters_alt_s2 = np.argwhere(np.isin(interactions_in_roi["cluster_id"], contributing_clusters_alt_s2))
-            result["in_alt_s2"][matching_clusters_alt_s2] = True
+                    for cluster_i, photons_i in zip(contributing_clusters_of_peak, photons_in_peak):
+                        mask = interactions_in_roi["cluster_id"] == cluster_i
+                        result["photons_in_" + peak_name][mask] = photons_i
 
         return result
