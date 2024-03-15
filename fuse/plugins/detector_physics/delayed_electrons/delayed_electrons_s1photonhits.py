e722188bd717c373234a740cbe616e1fa50eb09a
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2024-03-15 09:46:38 +0100

    fixing

--- fuse/plugins/detector_physics/delayed_electrons/delayed_electrons_s1photonhits.py
+++ fuse/plugins/detector_physics/delayed_electrons/delayed_electrons_s1photonhits.py
@@ -1,13 +1,11 @@
 import strax
+import straxen
 import logging
 import numpy as np
 
 export, __all__ = strax.exporter()
 
 from ....plugin import FuseBasePlugin
-
-logging.basicConfig(handlers=[logging.StreamHandler()])
-log = logging.getLogger('fuse.detector_physics.delayed_electrons.delayed_electrons_s1photonhits')
 
 @export
 class S1PhotonHitsEmpty(FuseBasePlugin):
