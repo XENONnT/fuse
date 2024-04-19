6d141ce9a1d88df21fab6f28f2a6232b0bb2a4ec
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2024-04-19 13:46:29 +0200

    Update dtype creation of SelectionMerger

--- fuse/plugins/micro_physics/cuts_and_selections/apply_selections.py
+++ fuse/plugins/micro_physics/cuts_and_selections/apply_selections.py
@@ -3,6 +3,13 @@ import numpy as np
 
 from ....plugin import FuseBasePlugin
 
+from ....dtypes import (
+    primary_positions_fields,
+    cluster_positions_fields,
+    cluster_id_fields,
+    cluster_misc_fields,
+)
+
 export, __all__ = strax.exporter()
 
 
@@ -18,25 +25,13 @@ class SelectionMerger(FuseBasePlugin):
     data_kind = "interactions_in_roi"
     __version__ = "0.0.1"
 
-    dtype = [
-        (("x position of the cluster [cm]", "x"), np.float32),
-        (("y position of the cluster [cm]", "y"), np.float32),
-        (("z position of the cluster [cm]", "z"), np.float32),
-        (("Energy of the cluster [keV]", "ed"), np.float32),
-        (("NEST interaction type", "nestid"), np.int8),
-        (("Mass number of the interacting particle", "A"), np.int8),
-        (("Charge number of the interacting particle", "Z"), np.int8),
-        (("Geant4 event ID", "evtid"), np.int32),
-        (("x position of the primary particle [cm]", "x_pri"), np.float32),
-        (("y position of the primary particle [cm]", "y_pri"), np.float32),
-        (("z position of the primary particle [cm]", "z_pri"), np.float32),
-        (("ID of the cluster", "cluster_id"), np.int32),
-        (("Xenon density at the cluster position", "xe_density"), np.float32),
-        (("ID of the volume in which the cluster occured", "vol_id"), np.int8),
-        (("Flag indicating if a cluster can create a S2 signal", "create_S2"), np.bool_),
-    ]
-
-    dtype = dtype + strax.time_fields
+    dtype = (
+        cluster_positions_fields
+        + cluster_id_fields
+        + cluster_misc_fields
+        + primary_positions_fields
+        + strax.time_fields
+    )
 
     def setup(self):
         # I can get the conditions in the volumes from the plugin lineage!
