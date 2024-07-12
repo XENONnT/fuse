f8e0b730198998aa2b33b960f7b0b6425c08f25d
cfuselli <cfuselli@nikhef.nl>
2024-07-12 08:03:23 -0500

    if gamma phot, give gamma - no matter what creaproc

--- fuse/plugins/micro_physics/lineage_cluster.py
+++ fuse/plugins/micro_physics/lineage_cluster.py
@@ -337,18 +337,12 @@ def classify_lineage(particle_interaction):
         elif particle_interaction["edproc"] == "conv":
             return NEST_BETA
         elif particle_interaction["edproc"] == "phot":
-            # Not sure about this, but was looking for a way to give beta yields
-            # to gammas that are coming directly from a radioactive decay
-            if particle_interaction["creaproc"] == "RadioactiveDecayBase":
-                return NEST_BETA
-            # Need this case for custom geant4 inputs...
-            elif particle_interaction["creaproc"] == "Null":
-                return NEST_BETA
-            else:
-                return NEST_GAMMA
+            # This is gamma photoabsorption. Return gamma
+            return NEST_GAMMA
         else:
             # could be rayleigh scattering or something else. Classify it as gamma...
             return NEST_BETA
+
 
     # Primaries and decay products
     elif (particle_interaction["creaproc"] == "RadioactiveDecayBase") or (
