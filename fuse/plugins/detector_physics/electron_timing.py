10330a0a0f2cb3817200fdd39399d803941268c0
Yongyu Pan <yongyu@midway2-login2.rcc.local>
2024-08-26 10:19:08 -0500

    implement_model

--- fuse/plugins/detector_physics/electron_timing.py
+++ fuse/plugins/detector_physics/electron_timing.py
@@ -40,16 +40,28 @@ class ElectronTiming(FuseBasePlugin):
         help="Time scale electrons are trapped at the liquid gas interface",
     )
 
-    # tf_electron_timing_near_wire_model = straxen.URLConfig(
-    #     default="take://resource://"
-    #     "SIMULATION_CONFIG_FILE.json?&fmt=json"
-    #     "&take=electron_trapping_time",
-    #     type=(int, float),
-    #     cache=True,
-    #     help="Time scale electrons are trapped at the liquid gas interface",
-    # )
-    # TODO: Add import of the TF model, can be a local file for test
 
+    tf_electron_timing_near_wire_model = straxen.URLConfig(
+        default="tf://"
+        "/home/yongyu/analysiscode_link/S2_shape_width/near_wire_model/Data_driven/"
+        "wired_model_temp_new.tar.gz",
+        cache=True,
+        help="test",
+    )
+
+    model_train_mean = straxen.URLConfig(
+        default="take://resource://"
+        "/project2/lgrandi/yongyu/data/data_v14/tf_model_input/x_train_mean_v1.npy?&fmt=npy",
+        cache=True,
+        help="test",
+    )
+
+    model_train_std = straxen.URLConfig(
+        default="take://resource://"
+        "/project2/lgrandi/yongyu/data/data_v14/tf_model_input/x_train_std_v1.npy?&fmt=npy",
+        cache=True,
+        help="test",
+    )
 
     perp_wires_cut_distance = straxen.URLConfig(
         default=4.45,
@@ -119,46 +131,68 @@ class ElectronTiming(FuseBasePlugin):
         x_obs,
         y_obs
     ):
+        time_grid = np.linspace(-30,30,300) # [us]
+
+        rot_x, rot_y = self.rotate_axis(x_obs, y_obs)
+        diff_wire = self.distance_to_wire(x_obs, y_obs)
+        
+        # Input for ML model
         time_r = np.repeat(time, n_electron.astype(np.int64))
-        print(len(time_r), time_r)
-        print(len(x_obs), x_obs)
+        x_obs_r = np.repeat(x_obs, n_electron.astype(np.int64))
+        y_obs_r = np.repeat(y_obs, n_electron.astype(np.int64))
+        x_rot_r = np.repeat(rot_x, n_electron.astype(np.int64))
+        y_rot_r = np.repeat(rot_y, n_electron.astype(np.int64))
+        diff_wire_r = np.repeat(diff_wire, n_electron.astype(np.int64))
+        # print(x_rot_r, y_rot_r, diff_wire_r)
+
         drift_time_mean_r = np.repeat(drift_time_mean, n_electron.astype(np.int64))
         drift_time_spread_r = np.repeat(drift_time_spread, n_electron.astype(np.int64))
 
-        timing = np.zeros(time_r.shape[0], dtype=np.int64)
-        mask_near_wires = self.perp_wire_region(x_obs, y_obs)
-        print(len(mask_near_wires))
+        timing = np.zeros(time_r.shape[0], dtype=np.float64)
+        mask_near_wires = self.perp_wire_region(x_obs_r, y_obs_r)
+        
         # For non-wire region, use diffusion constant map
-        # timing[~mask_near_wires] = self.rng.exponential(self.electron_trapping_time, size=time_r.shape[0])
-        # timing[~mask_near_wires] += self.rng.normal(drift_time_mean_r, drift_time_spread_r, size=time_r.shape[0])
-
-        timing = self.rng.exponential(self.electron_trapping_time, size=time_r.shape[0])
-        timing += self.rng.normal(drift_time_mean_r, drift_time_spread_r, size=time_r.shape[0])
-
+        timing[~mask_near_wires] = self.rng.exponential(self.electron_trapping_time, 
+                                                        size=time_r[~mask_near_wires].shape[0])
+        timing[~mask_near_wires] += self.rng.normal(drift_time_mean_r[~mask_near_wires], 
+                                                    drift_time_spread_r[~mask_near_wires], 
+                                                    size=time_r[~mask_near_wires].shape[0])
+        
         # For wire region, use data-driven TF model
-        # TODO: Add sampling from the TF model
-        # 1. inputting (rot_x, rot_y, distance to the wire, drift time [ns]) to the TF model, get (time grid, PDF) as output
-        # 2. Based on the PCF, convert to CDF, perform inverse 1D fit sampling, sample the electrons timing based on the number of electrons
-        # 3. Assign the sampled timing to timing
+        drift_time_temp = self.electron_trapping_time + drift_time_mean
+        drift_time_temp_r = np.repeat(drift_time_temp, n_electron.astype(np.int64))
+
+        # print(drift_time_temp)
+        self.model = self.tf_electron_timing_near_wire_model
+        input_model = np.column_stack((x_obs_r[mask_near_wires], 
+                                      y_obs_r[mask_near_wires], 
+                                      x_rot_r[mask_near_wires], 
+                                      y_rot_r[mask_near_wires], 
+                                      diff_wire_r[mask_near_wires], 
+                                      drift_time_temp_r[mask_near_wires]))
+        input_model = (input_model - self.model_train_mean) / self.model_train_std
+
+        pdf = self.model(input_model)
+        cdf = np.cumsum(pdf, axis=1)
+        cdf = cdf / cdf[:, -1].reshape(-1, 1)  # normalization
+        timing_temp = np.array([np.interp(np.random.rand(), cdf[i], time_grid) 
+                                for i in range(cdf.shape[0])])
+        timing_temp *= 1e3 # convert us to ns
+        timing[mask_near_wires] = drift_time_temp_r[mask_near_wires] + timing_temp
 
         return time_r + timing.astype(np.int64)
 
+    def rotate_axis(self, x_obs, y_obs):
+        x_rot = np.cos(self.perp_wire_angle) * x_obs - np.sin(self.perp_wire_angle) * y_obs
+        y_rot = np.sin(self.perp_wire_angle) * x_obs + np.cos(self.perp_wire_angle) * y_obs
+        return x_rot, y_rot
 
+    def distance_to_wire(self, x_obs, y_obs):
+        x_rot, y_rot = self.rotate_axis(x_obs, y_obs)
+        return np.abs(np.abs(x_rot) - self.perp_wire_x_pos)
+    
     def perp_wire_region(self, x_obs, y_obs):
-        """
-        Returns a mask selecting the events near the perpendicular wires.
-        """
-
-        def rotate_axis(x_obs, y_obs):
-            x_rot = np.cos(self.perp_wire_angle) * x_obs - np.sin(self.perp_wire_angle) * y_obs
-            y_rot = np.sin(self.perp_wire_angle) * x_obs + np.cos(self.perp_wire_angle) * y_obs
-            return x_rot, y_rot
-
-        def get_near_wires(x_obs, y_obs, distance_cm):
-            """Returns a mask selecting the events near the perpendicular wires."""
-            x_rot, y_rot = rotate_axis(x_obs, y_obs)
-            mask_near_wires = np.abs(np.abs(x_rot) - self.perp_wire_x_pos) < distance_cm
-            return mask_near_wires
-
-        return get_near_wires(x_obs, y_obs, self.perp_wires_cut_distance)
+        distance_cm = 5
+        mask_near_wires = self.distance_to_wire(x_obs, y_obs) < distance_cm
+        return mask_near_wires
 
