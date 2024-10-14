4d309052f00691c6d6ac8220c336ed9cc53d14ec
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2024-10-14 09:58:42 +0200

    Add alt fields and extend the docstring.

--- fuse/plugins/processing/corrected_areas.py
+++ fuse/plugins/processing/corrected_areas.py
@@ -8,8 +8,14 @@ export, __all__ = strax.exporter()
 class CorrectedAreasMC(straxen.CorrectedAreas):
     """Corrected areas plugin for MC data.
 
-    This plugin overwrites the cs1 and cs2 fields with the not-time-
-    corrected values as the effects are not simulated in the MC.
+    This plugin overwrites the (alt_)cs1 and (alt_)cs2 fields with the
+    not-time- corrected values as the effects of time dependent single-
+    electron gain, extraction efficiency and photo ionization are not
+    simulated in fuse.
+
+    Applying corrections for these effects to simulated data would lead
+    to incorrect results. This plugins should only be used for simulated
+    data!
     """
 
     __version__ = "0.0.1"
@@ -19,5 +25,8 @@ class CorrectedAreasMC(straxen.CorrectedAreas):
         result = super().compute(events)
         result["cs1"] = result["cs1_wo_timecorr"]
         result["cs2"] = result["cs2_wo_timecorr"]
+
+        result["alt_cs1"] = result["alt_cs1_wo_timecorr"]
+        result["alt_cs2"] = result["alt_cs2_wo_timecorr"]
 
         return result
