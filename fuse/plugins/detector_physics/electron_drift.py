0b301d10d1cbdbd77d9d1f7c63481edc5a4cde81
Yongyu Pan <yongyu@midway2-login2.rcc.local>
2024-11-21 01:53:01 -0600

    substraction_upper_diffusion

--- fuse/plugins/detector_physics/electron_drift.py
+++ fuse/plugins/detector_physics/electron_drift.py
@@ -357,6 +357,7 @@ class ElectronDrift(FuseBasePlugin):
         drift_time_above_gate = self.drift_time_gate
 
         drift_time_mean = drift_time_below_gate + drift_time_above_gate
+
         drift_time_mean = np.clip(drift_time_mean, 0, np.inf)
 
         drift_time_spread_below_gate_squared = (
@@ -373,6 +374,18 @@ class ElectronDrift(FuseBasePlugin):
         )
         drift_time_spread = np.sqrt(
             drift_time_spread_below_gate_squared + drift_time_spread_above_gate_squared
+        )
+
+        # if pp wire s2 width simulation (substract ):
+        drift_time_mean = drift_time_below_gate + drift_time_above_gate - (3.8 / drift_velocity_below_gate)
+        drift_time_spread_38mm = (
+            2
+            * diffusion_constant_longitudinal
+            * (3.8 / drift_velocity_below_gate)
+            / drift_velocity_below_gate**2
+        )
+        drift_time_spread = np.sqrt(
+            drift_time_spread_below_gate_squared + drift_time_spread_above_gate_squared - drift_time_spread_38mm
         )
 
         return drift_time_mean, drift_time_spread
