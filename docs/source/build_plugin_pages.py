d78e26cdc1d10c19884f334ed96fade0d90707fe
cfuselli <cfuselli@nikhef.nl>
2025-04-10 10:48:05 -0500

    use publi config context in examples

--- docs/source/build_plugin_pages.py
+++ docs/source/build_plugin_pages.py
@@ -173,11 +173,7 @@ def create_plugin_documentation_text(st, plugin):
 
 def build_all_pages():
 
-    st = fuse.context.xenonnt_fuse_full_chain_simulation(
-        output_folder="./fuse_data",
-        run_without_proper_corrections=True,
-        simulation_config="sr0_dev",
-    )
+    st = fuse.context.public_config_context()
 
     all_registered_fuse_plugins = {}
     for key, value in st._plugin_class_registry.items():
