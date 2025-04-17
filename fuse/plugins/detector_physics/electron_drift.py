826f7283c68e23aa9ffbd709db4d6221ec086f84
cfuselli <cfuselli@nikhef.nl>
2025-04-17 03:51:47 -0500

    clip positions

--- fuse/plugins/detector_physics/electron_drift.py
+++ fuse/plugins/detector_physics/electron_drift.py
@@ -215,6 +215,15 @@ class ElectronDrift(FuseBasePlugin):
 
             self.diffusion_longitudinal_map = _rz_map
 
+
+
+        csys = self.fdc_map_fuse.coordinate_system
+        rmin, rmax = csys[:, 0].min(), csys[:, 0].max()
+        zmin, zmax = csys[:, 1].min(), csys[:, 1].max()
+        self.fdc_map_r_bounds = (rmin, rmax)
+        self.fdc_map_z_bounds = (zmin, zmax)
+
+
     def compute(self, interactions_in_roi):
         # Just apply this to clusters with photons
         mask = interactions_in_roi["electrons"] > 0
@@ -317,7 +326,16 @@ class ElectronDrift(FuseBasePlugin):
         """
         positions = np.array([np.sqrt(x**2 + y**2), z]).T
         theta = np.arctan2(y, x)
-        r_obs = self.fdc_map_fuse(positions, map_name="r_distortion_map")
+
+        clipped_positions = np.copy(positions)
+        clipped_positions[:, 0] = np.clip(
+            clipped_positions[:, 0], self.fdc_map_r_bounds[0], self.fdc_map_r_bounds[1]
+        )
+        clipped_positions[:, 1] = np.clip(
+            clipped_positions[:, 1], self.fdc_map_z_bounds[0], self.fdc_map_z_bounds[1]
+        )
+
+        r_obs = self.fdc_map_fuse(clipped_positions, map_name="r_distortion_map")
         x_obs = r_obs * np.cos(theta)
         y_obs = r_obs * np.sin(theta)
 
